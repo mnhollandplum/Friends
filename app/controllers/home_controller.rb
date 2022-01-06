c53887114d5dc7188122e01e88febd8fc7361bc9
Nikki Holland-Plum <mnhollandplum@gmail.com>
2022-01-05 17:03:00 -0700

    Add resume to about me page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   end
 
   def about
+    @about_me = "My name is Nikki Holland-Plum. I am a Software Engineer and this is one of my simple ROR projects I'm working on."
   end
 end
